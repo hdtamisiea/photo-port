5dea9dc1b454e8a442e993370520d99a24eb608a
htam <hdfair@gmail.com>
2022-05-05 12:10:05 -0400

    added tests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
+import React from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
 
 function App() {
   return (
