a30c1ffd32b1bbd6c1904774cd24bdab080c38dd
htam <hdfair@gmail.com>
2022-05-06 10:48:34 -0400

    finished project

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -4,16 +4,17 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
+afterEach(cleanup);
+
 describe('About component', () => {
   it('renders', () => {
     render(<About />);
   });
-  
-  it('matches snapshot DOM node structure', () => {
+
+  it('matches snapshot', () => {
     const { asFragment } = render(<About />);
-    
+
     expect(asFragment()).toMatchSnapshot();
   });
 })
 
-afterEach(cleanup);
